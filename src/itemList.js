f65f6ad5852d520b3d61603046a59e5eef3912a4
Sam Cherry <76022735+samuelcherry@users.noreply.github.com>
2025-09-28 10:54:22 -0600

    added specific data and icons. Next step, set up database to save unlock/lock state

--- src/itemList.js
+++ src/itemList.js
@@ -16,74 +16,86 @@ const questItems = [
   {
     id: 3,
     title: "Spiked boots",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    icon: "/questItemImages/Spiked_boots.png",
+    unlocked: "no",
+    quest: "Desert Treasure I"
   },
   {
     id: 4,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Trowel",
+    icon: "/questItemImages/Trowel.png",
+    unlocked: "no",
+    quest: "Another Slice of H.A.M."
   },
   {
     id: 5,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Battered book",
+    icon: "/questItemImages/Battered_book.png",
+    unlocked: "no",
+    quest: "Elemental Workshop II"
   },
   {
     id: 6,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Battered key",
+    icon: "/questItemImages/Battered_key.png",
+    unlocked: "no",
+    quest: "Elemental Workshop II"
   },
   {
     id: 7,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Fake beard",
+    icon: "/questItemImages/Fake_beard.png",
+    unlocked: "no",
+    quest: "Wanted!, Forgettable Tale..."
   },
   {
     id: 8,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Red hot sauce",
+    icon: "/questItemImages/Red_hot_sauce.png",
+    unlocked: "no",
+    quest: "My Arm's Big Adventure"
   },
   {
     id: 9,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Red vine worm",
+    icon: "/questItemImages/Red_vine_worm.png",
+    unlocked: "no",
+    quest: "Fairy Tale I - Growing Pains"
   },
   {
     id: 10,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Dramen branch",
+    icon: "/questItemImages/Dramen_branch.png",
+    unlocked: "no",
+    quest: "The Knight's Sword"
   },
   {
     id: 11,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Black mushroom",
+    icon: "/questItemImages/Black_mushroom.png",
+    unlocked: "yes",
+    quest: "Shadow of the Storm"
   },
   {
     id: 12,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Strange implement",
+    icon: "/questItemImages/Strange_implement.png",
+    unlocked: "no",
+    quest: "Shadow of the Storm"
   },
   {
     id: 13,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Black Dye",
+    icon: "/questItemImages/Black_dye.png",
+    unlocked: "no",
+    quest: "Shadow of the Storm"
   },
   {
     id: 14,
-    title: "redberry pie",
-    icon: "/questItemImages/Redberry_pie.png",
-    unlocked: "no"
+    title: "Karamjan rum",
+    icon: "/questItemImages/Karamjan_rum.png",
+    unlocked: "no",
+    quest: "Tai Bwo Wannai Trio"
   }
 ];
 
