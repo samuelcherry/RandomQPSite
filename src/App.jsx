a7d72261cbe14ba1b73695527791dd0c4680765b
Sam Cherry <76022735+samuelcherry@users.noreply.github.com>
2025-09-28 08:33:09 -0600

    set up grid and unlock/ locked styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import questItems from "./itemList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <h1> Random QP</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input className="searchBar"></input>
+        <button>Search</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <button>unlocked</button>
+        <button>locked</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="gridContainer">
+        {questItems.map((item) => (
+          <div
+            key={item.id}
+            className={
+              item.unlocked === "yes" ? "card unlocked" : "card locked"
+            }
+          >
+            <img src={item.icon} alt={item.title} title={item.title} />
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/itemList.js
@@ -0,0 +1,90 @@
+const questItems = [
+  {
+    id: 1,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no",
+    quest: "The Knight's Sword"
+  },
+  {
+    id: 2,
+    title: "Ogre bellows",
+    icon: "/questItemImages/Ogre_bellows.png",
+    unlocked: "yes",
+    quest: "Recipe For Disaster, Mourning's End Part I"
+  },
+  {
+    id: 3,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 4,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 5,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 6,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 7,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 8,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 9,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 10,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 11,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 12,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 13,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  },
+  {
+    id: 14,
+    title: "redberry pie",
+    icon: "/questItemImages/Redberry_pie.png",
+    unlocked: "no"
+  }
+];
+
+export default questItems;
